72eb18541ad8c4ee38dfdc4a211fb9fae192cc8b
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-10-28 00:49:17 +0300

    categories loaded

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import './App.css';
+import CategoryItem from "./components/category-item/category-item.component";
 
-function App() {
+import "./categories.styles.scss";
+
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Womens",
+    },
+    {
+      id: 5,
+      title: "Mens",
+    },
+  ];
+
   return (
-    <div >
-      
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
