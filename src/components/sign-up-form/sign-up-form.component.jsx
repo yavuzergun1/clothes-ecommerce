0a63e58b85deaabae9981976c69a7605de089f98
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-11-02 18:09:27 +0300

    button styling

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import FormInput from "../form-input/form-input.component";
-// import Button from "../button/button.component";
+import Button from "../button/button.component";
 
 import {
   createAuthUserWithEmailAndPassword,
@@ -96,7 +96,7 @@ const SignUpForm = () => {
           name="confirmPassword"
           value={confirmPassword}
         />
-        <button type="submit">Sign Up</button>
+        <Button  type="submit">Sign Up</Button>
       </form>
     </div>
   );
