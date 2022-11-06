ddb89f138326fe04ec563fef940cb97c21615966
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-11-06 19:57:23 +0300

    cart count setted with reduce method

--- src/contexts/cart.context.js
+++ src/contexts/cart.context.js
@@ -1,6 +1,4 @@
-import { createContext, useState } from "react";
-
-
+import { createContext, useState, useEffect } from "react";
 
 export const CartContext = createContext({
   isCartOpen: false,
@@ -12,16 +10,34 @@ export const CartContext = createContext({
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState();
 
+  useEffect(() => {
+    const itemCount = cartItems.reduce(
+      (total, cartItem) => (total = total + cartItem.quantity),
+      0
+    );
+    setCartCount(itemCount);
+  }, [cartItems]);
+  console.log(cartCount);
   const addItemToCart = (product) => {
-    setCartItems(addControl(cartItems, product))} ;
-    console.log("cart items",cartItems);
+    setCartItems(addControl(cartItems, product));
+  };
+  console.log("cart items", cartItems);
 
-  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart };
+  const values = {
+    isCartOpen,
+    setIsCartOpen,
+    cartItems,
+    addItemToCart,
+    cartCount,
+    setCartCount,
+  };
 
-  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+  return <CartContext.Provider value={values}>{children}</CartContext.Provider>;
 };
-const addControl = (cartItems, productToAdd) => {
+
+export const addControl = (cartItems, productToAdd) => {
   const existingCartItem = cartItems.find(
     (cartItem) => cartItem.id === productToAdd.id
   );
@@ -35,4 +51,3 @@ const addControl = (cartItems, productToAdd) => {
 
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
- 
