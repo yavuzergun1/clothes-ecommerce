4c3c439843a398611935173c7381a4af4bdb7594
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-12-21 20:56:45 +0300

    title router setted

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,12 +1,15 @@
 import "./category-preview.styles.scss";
 import React from "react";
 import ProductCard from "../product-card/product-card.component";
+import { Link } from "react-router-dom";
 
 function CategoryPreview({ title, products }) {
   return (
     <div className="category-preview-container">
       <h2>
-        <span className="title">{title.toUpperCase()} </span>
+        <Link to={title} className="title">
+          {title.toUpperCase()}{" "}
+        </Link>
       </h2>
       <div className="product-cards-container">
         {products
