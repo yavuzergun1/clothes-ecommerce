d497d9138bede964771b6b9490e210f5b23f9484
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-12-21 23:23:37 +0300

    category component added

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,6 +1,6 @@
 import "./category.styles.scss";
 import { useParams } from "react-router-dom";
-import { useContext, useState } from "react";
+import { Fragment, useContext, useState } from "react";
 import { CategoriesContext } from "../../contexts/categories.context";
 import ProductCard from "../../components/product-card/product-card.component";
 import { useEffect } from "react";
@@ -9,19 +9,22 @@ function Category() {
   const { category } = useParams();
   const { categories } = useContext(CategoriesContext);
   const [products, setProducts] = useState();
-  console.log("CATEGORY",categories[category]);
+  console.log("CATEGORY", categories[category]);
 
   useEffect(() => {
     setProducts(categories[category]);
   }, [category, categories]);
 
   return (
-    <div className="category-container">
-      {products &&
-        products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-    </div>
+    <Fragment>
+      <h2 className="category-title">{category.toUpperCase()}</h2>
+      <div className="category-container">
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </Fragment>
   );
 }
 
