33f2386a9f2fb5e2eb149dfbedb0ec65368eea85
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-12-23 22:21:05 +0300

    button styled component added

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,5 +1,5 @@
 import "./product-card.styles.scss";
-import Button from "../button/button.component";
+import Button, {BUTTON_TYPE_CLASSES} from "../button/button.component";
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 
@@ -15,7 +15,7 @@ const ProductCard = ({ product }) => {
         <span className="price">{price}</span>
       </div>
 
-      <Button buttonType="inverted" onClick={() => addItemToCart(product)}>
+      <Button buttonType={BUTTON_TYPE_CLASSES.inverted} onClick={() => addItemToCart(product)}>
         Add to card
       </Button>
     </div>
